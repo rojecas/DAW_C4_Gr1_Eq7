e8375f557bef3064f229d42a08067215fc411fb2
rojecas <rojecas@gmail.com>
2022-11-13 12:25:12 -0500

    ajuste del formulario login, capturar usr, pass

--- FrontEndMF/src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ FrontEndMF/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+const cryptoJS=require('cryptojs')
 
 @Component({
   selector: 'app-identificacion',
@@ -7,13 +8,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./identificacion.component.css']
 })
 export class IdentificacionComponent implements OnInit {
+
   fgValidador: FormGroup = this.fb.group({
     'usuario':['',[Validators.required, Validators.email]],
     'clave':['',[Validators.required]]
   });
-  constructor(private fb: FormBuilder) { }
+
+  constructor(private fb: FormBuilder) { 
+  }
 
   ngOnInit(): void {
   }
-
+  IdentificarUsuario(){
+    let usuario= this.fgValidador.controls['usuario'].value;
+    let clave= this.fgValidador.controls['clave'].value;
+    let claveCifrada = cryptoJS.MD5;
+    alert(usuario)
+    alert(clave)
+  }
 }
